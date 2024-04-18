a1cc960fee35dd74da30c0228fdb15e1fde2bdf1
Nyikoe04 <163550148+Nyikoe04@users.noreply.github.com>
2024-04-18 20:56:25 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,34 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+   string name;
+  int age;
+ char gender;
+
+ cout <<"Enter your name";
+ cin>>name;
+
+ cout<<"Enter your age";
+ cin>>age;
+
+ cout<<"Enter your gender(m/f)";
+  cin>> gender;}
+
+  if(age>=26 && patner age>=26){
+    if(gender!= patner gender){
+        cout<<"congratulations,"<<name<<"!You and"<<patner name<<"seem to be matching";<<endl;
+
+    }
+  } else {
+  cout<<"sorry"<< name <<" you or your patner do not fit to be in a relatioship";<< endl;
+
+  //output partner recommendation
+  cout<<"\nbased on your age and patner name, we recommend finding someone"<<patnerAgeRange<<"Who is"<<patnername<<"\n"
+  }
+  return 0;
+  }
